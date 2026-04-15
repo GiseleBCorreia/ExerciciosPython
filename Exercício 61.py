60e86f50788215c976a143a9f9fadc16b088bd6e
Gisele Correia <giselehmcorreia@gmail.com>
2026-04-15 18:00:33 -0300

    Update Exercício 61.py

--- Exercício 61.py
+++ Exercício 61.py
@@ -2,13 +2,17 @@ contador = 0
 soma = 0
 
 while True:
-    numero = int(input("Digite um número (999 para parar): "))
-    
-    if numero == 999:
-        break
-    
-    contador += 1
-    soma += numero
 
-print("Quantidade de números digitados:", contador)
-print("Soma dos valores:", soma)
+    numero = int(input("Digite um número (Valor negativo interrompe o programa): "))
+
+    if numero<0:
+        break
+
+    print(f'A tabuada do {numero} é:')
+
+    for i in range(1, 11):
+        print(f'{numero} x {i} = {numero * i}')
+    
+    
+print('Fim do programa')
+    
