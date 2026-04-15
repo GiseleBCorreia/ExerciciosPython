efd69a2c3465b2232a576916f5721940bea8bb47
Gisele Correia <giselehmcorreia@gmail.com>
2026-04-15 17:52:19 -0300

    Create Exercício 61.py

---
+++ Exercício 61.py
@@ -0,0 +1,14 @@
+contador = 0
+soma = 0
+
+while True:
+    numero = int(input("Digite um número (999 para parar): "))
+    
+    if numero == 999:
+        break
+    
+    contador += 1
+    soma += numero
+
+print("Quantidade de números digitados:", contador)
+print("Soma dos valores:", soma)
