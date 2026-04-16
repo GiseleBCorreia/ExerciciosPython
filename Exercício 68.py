606b1a37c06533b988e3726b103cbf3fa76b2051
Gisele Correia <giselehmcorreia@gmail.com>
2026-04-16 07:01:11 -0300

    Add initial Par ou Ímpar game implementation
    Initial implementation of a game that plays Par ou Ímpar with random number generation and user input.

---
+++ Exercício 68.py
@@ -0,0 +1,13 @@
+import random
+c=random.randit(1,10)
+print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+print('VAMOS JOGAR PAR OU ÍMPAR')
+print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+import random
+c=random.randit(1,10)
+n1=int(input('Digite um valor:')
+for x in n1:
+  a2=str(input('Par ou ímpar [P/I]')).strip().upper()
+    if 
+  print('------------------------------')
+  print(f'Você jogou {n1} e o computador jogou {c} total deu {n1+c} e esse valor é {a3}')
