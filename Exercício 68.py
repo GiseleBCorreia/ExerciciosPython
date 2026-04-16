08be858a9082535ca2e3979b9c6adb77e3bab24a
Gisele Correia <giselehmcorreia@gmail.com>
2026-04-16 07:06:27 -0300

    Update Exercício 68.py

--- Exercício 68.py
+++ Exercício 68.py
@@ -1,13 +1,13 @@
-import random
-c=random.randit(1,10)
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
 print('VAMOS JOGAR PAR OU ÍMPAR')
 print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
 import random
 c=random.randit(1,10)
 n1=int(input('Digite um valor:')
+a2=str(input('Par ou ímpar [P/I]')).strip().upper()
 for x in n1:
-  a2=str(input('Par ou ímpar [P/I]')).strip().upper()
-    if 
-  print('------------------------------')
-  print(f'Você jogou {n1} e o computador jogou {c} total deu {n1+c} e esse valor é {a3}')
+    if [(n1+c)%2]=0:
+      print('------------------------------')
+      print(f'Você jogou {n1} e o computador jogou {c} total deu {n1+c} e esse valor é par.')
+    else:
+      print(f'Você jogou {n1} e o computador jogou {c} total deu {n1+c} e esse valor é ímpar.')
